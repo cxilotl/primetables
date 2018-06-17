d7c50024b9fd5bada305257b2ba75abaf62d8797
Carlos Xilotl <cxilotl@aol.com>
2018-06-17 20:15:44 +0100

    Setting up initial testing framework structure

---
+++ test/primeTablesTests.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+
+describe('Prime Tables', function() {
+
+    describe('Given a numeric input N, ', function() {
+
+        let n;
+
+        it('When N is 1, it should output a multiplication table for one prime', function() {
+
+            n = 1;
+
+            assert(true, true, "are equals");
+
+        });
+
+    });
+
+});
